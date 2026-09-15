64cf3e1ec493a0e488bc48e0f593060e84abacbe
enzo <enzo@design-prism.com>
2026-09-15 08:30:58 -0700

    Add historic Canary Cove leads chunk D

---
+++ data/submissions-old-b.ts
@@ -0,0 +1,59 @@
+import type { Submission } from "./submission-types";
+
+export const oldBSubmissions: Submission[] = [
+  {
+    id: "b-20260424-rich-schones",
+    type: "booking",
+    name: "Rich Schones",
+    email: "schonesfamily@gmail.com",
+    phone: "+1 949-394-1213",
+    date: "2026-04-24T18:49:44.604Z",
+    arrival: "2026-09-18",
+    departure: "2026-09-24",
+    guests: "6 adults",
+    message: "Parents, 2 daughters and their husbands for my wifes birthday. Looking for price",
+    referral: "google",
+    sourceForm: "booking"
+  },
+  {
+    id: "c-20260424-val-kaye",
+    type: "contact",
+    name: "Val Kaye",
+    email: "val@sisterhoodtravels.com",
+    date: "2026-04-24T16:58:40.061Z",
+    message: "Hello, I am following up to my previous email for information please Val Kaye Travel Planner Sisterhood Travels",
+    sourceForm: "contact"
+  },
+  {
+    id: "b-20260424-erin-jones",
+    type: "booking",
+    name: "Erin Jones",
+    email: "erinj58@yahoo.com",
+    phone: "+1 470-460-0412",
+    date: "2026-04-24T06:14:57.611Z",
+    arrival: "2027-04-15",
+    departure: "2027-04-19",
+    guests: "14 adults",
+    message: "family vacation",
+    referral: "google",
+    sourceForm: "booking"
+  },
+  {
+    id: "c-20260421-val-kaye",
+    type: "contact",
+    name: "Val Kaye",
+    email: "val@sisterhoodtravels.com",
+    date: "2026-04-21T19:58:19.950Z",
+    message: "Hello- We are interested in a possible buyout of your location for a women's group trip in January, 2028, (possibly the last week in January. There are a lot of logistics and questions we have to see if this is the right fit for our tour and we have amany questions on pricing and what is included and what is not. Do. you have a person I can work with to coordinate with? Thank you Val Kaye Sisterhood Travels val@sisterhoodtravels.com",
+    sourceForm: "contact"
+  },
+  {
+    id: "c-20260420-lori-devine",
+    type: "contact",
+    name: "Lori Devine",
+    email: "lori@marallinace.org",
+    date: "2026-04-20T21:15:35.493Z",
+    message: "Hello, I am working with MarAlliance, and Canary Cove is listed as the preferred accommodations for our February trips. I am attempting to automate our system for reservations and payments by using the WeTravel platform. Is there someone I can email an invitation to? I also need to check availability and rates for Feb 20-27th 2027. Please feel free to reach out to me. I don't have much information in my notes. Thank you! Lori Devine Expeditions Coordinator",
+    sourceForm: "contact"
+  }
+];
