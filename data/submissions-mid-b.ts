14300922faad235336eeeb3826a0f391c37d8375
enzo <enzo@design-prism.com>
2026-09-15 08:30:11 -0700

    Add historic Canary Cove leads chunk B

---
+++ data/submissions-mid-b.ts
@@ -0,0 +1,84 @@
+import type { Submission } from "./submission-types";
+
+export const midBSubmissions: Submission[] = [
+  {
+    id: "b-20260613-lyles-eddins",
+    type: "booking",
+    name: "Lyles Eddins",
+    email: "virginialyles@gmail.com",
+    phone: "+1 704-458-5634",
+    date: "2026-06-13T18:08:32.773Z",
+    arrival: "2027-03-19",
+    departure: "2027-03-25",
+    guests: "4 adults, 4 children ages 13 and 17",
+    message: "Family trip",
+    pagePath: "/book",
+    referral: "other-search",
+    sourceForm: "booking"
+  },
+  {
+    id: "c-20260603-natalie-peterson",
+    type: "contact",
+    name: "Natalie Peterson",
+    email: "nataliempeterson@gmail.com",
+    date: "2026-06-03T10:51:52.610Z",
+    message: "Hello! We are a group of 6 women traveling from different parts of the US to celebrate our 50th birthdays next year. We are looking to travel February 11-15, 2027. I would appreciate to hear estimates of the rates/costs for us at that time please. What is included and how do you estimate extra costs such as groceries and excursions pls? Thank you!",
+    pagePath: "/",
+    referrer: "https://www.google.com/",
+    sourceForm: "contact"
+  },
+  {
+    id: "c-20260601-bryant-craig",
+    type: "contact",
+    name: "Bryant Craig",
+    email: "craigdvm@gmail.com",
+    date: "2026-06-01T12:25:53.860Z",
+    message: "I have sent several booking requests and called last week. Just checking to see what the status of my request is currently. They were sent with the email craigdvm@gmail.com, you can send directly back to that email or call me at 580-541-7004 Bryant Craig",
+    pagePath: "/contact",
+    referrer: "https://www.canarycove.com/book",
+    sourceForm: "contact"
+  },
+  {
+    id: "b-20260526-bryant-craig",
+    type: "booking",
+    name: "Bryant Craig",
+    email: "craigdvm@gmail.com",
+    phone: "+1 580-541-7004",
+    date: "2026-05-26T15:39:23.247Z",
+    arrival: "2026-09-12",
+    departure: "2026-09-19",
+    guests: "6 adults",
+    message: "We will have 6 for the majority of the stay but might just have 2-4 for part of it.",
+    pagePath: "/book",
+    referral: "other",
+    referrer: "https://www.bing.com/",
+    sourceForm: "booking"
+  },
+  {
+    id: "b-20260525-cyndra-crossman",
+    type: "booking",
+    name: "Cyndra Crossman",
+    email: "cyndra.rae@yahoo.com",
+    phone: "+1 602-503-1919",
+    date: "2026-05-25T23:56:59.135Z",
+    arrival: "2027-01-28",
+    departure: "2027-02-02",
+    guests: "12 adults, 1 child age 4",
+    message: "40th birthday celebration Not exactly sure how many people but maybe 10-12?",
+    pagePath: "/book",
+    referral: "google",
+    referrer: "https://www.google.com/",
+    sourceForm: "booking"
+  },
+  {
+    id: "c-20260520-bryant-craig",
+    type: "contact",
+    name: "Bryant Craig",
+    email: "craigdvm@gmail.com",
+    date: "2026-05-20T15:03:09.939Z",
+    message: "We are looking to book for September 11th to the 18th, can you let us know if this is available?",
+    pagePath: "/contact",
+    referrer: "https://www.bing.com/",
+    sourceForm: "contact"
+  }
+];
