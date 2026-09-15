10e3656473f829436648266513322a6e149f2069
enzo <enzo@design-prism.com>
2026-09-15 08:30:37 -0700

    Add historic Canary Cove leads chunk C

---
+++ data/submissions-old-a.ts
@@ -0,0 +1,71 @@
+import type { Submission } from "./submission-types";
+
+export const oldASubmissions: Submission[] = [
+  {
+    id: "b-20260516-bryant-craig",
+    type: "booking",
+    name: "Bryant Craig",
+    email: "craigdvm@gmail.com",
+    phone: "+1 580-541-7004",
+    date: "2026-05-16T13:18:46.038Z",
+    arrival: "2026-09-12",
+    departure: "2026-09-19",
+    guests: "4 adults",
+    message: "Slow pace- fishing, snorkeling, sight seeing",
+    pagePath: "/book",
+    referral: "other",
+    referrer: "https://duckduckgo.com/",
+    sourceForm: "booking"
+  },
+  {
+    id: "b-20260502-bryant-craig",
+    type: "booking",
+    name: "Bryant Craig",
+    email: "craigdvm@gmail.com",
+    phone: "+1 580-541-7004",
+    date: "2026-05-02T15:45:08.185Z",
+    arrival: "2026-09-13",
+    departure: "2026-09-19",
+    guests: "6 adults",
+    message: "Relax and fun with friends. Will want to snorkel a little and one or more will want to do a couple days of fishing with a guide.",
+    referral: "other",
+    sourceForm: "booking"
+  },
+  {
+    id: "b-20260429-blade-cruickshank",
+    type: "booking",
+    name: "Blade Cruickshank",
+    email: "blade.cruickshank@gmail.com",
+    phone: "+1 405-434-8827",
+    date: "2026-04-29T19:23:57.943Z",
+    arrival: "2026-10-31",
+    departure: "2026-11-07",
+    guests: "6 adults",
+    message: "Hi Canary Cove Team! I'm so excited to try and coordinate a trip to come back. We are looking at 3 couples. We will probably do snorkeling and maybe a fishing day and day or two in town. We are considering renting a golf cart to explore a few spots but maybe only a couple of the days.",
+    referral: "other",
+    sourceForm: "booking"
+  },
+  {
+    id: "c-20260429-val-kaye",
+    type: "contact",
+    name: "Val Kaye",
+    email: "val@sisterhoodtravels.com",
+    date: "2026-04-29T16:29:36.962Z",
+    message: "Hello, this is my 3rd email request for a reach out for information to book your establishment and I have not heard back from anyone. I have called both phone numbers and no answer. I am hoping to get some information from you for our group. If someone could please replay I would appreciate it. Thank you, Val Kaye",
+    sourceForm: "contact"
+  },
+  {
+    id: "b-20260427-jon-kurtyka",
+    type: "booking",
+    name: "Jon Kurtyka",
+    email: "jonkurtyka@yahoo.com",
+    phone: "+1 215-292-1040",
+    date: "2026-04-27T15:46:02.589Z",
+    arrival: "2026-05-21",
+    departure: "2026-05-24",
+    guests: "2 adults",
+    message: "Birthday celebration",
+    referral: "other",
+    sourceForm: "booking"
+  }
+];
